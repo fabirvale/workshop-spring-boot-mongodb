c8016dcaa7129e18c6c39e2bcdca6b03e0566038
Fabiana Vale <fabianavale91@gmail.com>
2025-10-02 14:54:16 -0300

    Consulta simples com query methods

--- src/main/java/com/fabirvale/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fabirvale/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.fabirvale.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,6 @@ import com.fabirvale.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-	
-
+	 
+     List<Post> findByTitleContainingIgnoreCase(String text);
 }
